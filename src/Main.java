dd833b3dbd7ffb1843a44b9a106d3be9a2f24ba3
fowl96User <fowl69@yandex.ru>
2023-03-19 22:31:11 +0300

    задача к билету №2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    String palindrom = "saippuakivikauppias";
+    boolean result = true;
+        for (int i = 0; i < palindrom.length(); i++) {
+            if (palindrom.charAt(i) != palindrom.charAt(palindrom.length()- 1 - i)) {
+                result = false;
+            }
+        }
+        if (result) {
+            System.out.println("палиндром");
+        } else {
+            System.out.println("не палиндром");
+        }
     }
 }
